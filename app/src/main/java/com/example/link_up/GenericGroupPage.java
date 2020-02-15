39289ab51744c714ce0de7d8ce124065cae66c2d
oshalash38 <omarshalash@live.com>
2020-02-15 15:43:15 -0500

    Added GenericPage

---
+++ app/src/main/java/com/example/link_up/GenericGroupPage.java
@@ -0,0 +1,14 @@
+package com.example.link_up;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GenericGroupPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_generic_group_page);
+    }
+}
